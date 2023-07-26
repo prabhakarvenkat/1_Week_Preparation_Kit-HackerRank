0ccc80f0492b58d525a8a2647a7a62f6c6d76b2f
PRABHAKAR V <117185272+prabhakarvenkat@users.noreply.github.com>
2023-07-26 19:42:48 +0530

    Tower Breakers.py

---
+++ Day_3/Tower Breakers.py
@@ -0,0 +1,10 @@
+T = int(input())
+for t in range(T):
+    n, m = [int(x) for x in input().strip().split()]
+    if m == 1: 
+        print(2)
+    else:
+        if n % 2 == 1:
+            print(1)
+        else:
+            print(2)
